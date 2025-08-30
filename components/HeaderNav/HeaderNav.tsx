b445847ae072fb82a94013cda646af149f993746
jung hyun, ko <jeremyko69@gmail.com>
2025-08-31 00:29:42 +0900

    refactoring

--- components/HeaderNav/HeaderNav.tsx
+++ components/HeaderNav/HeaderNav.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
@@ -6,35 +6,48 @@ import Link from "next/link";
 import Search from "../Search/Search";
 
 import { Suspense } from "react";
-// useSearchParams() should be wrapped in a suspense boundary at page "/404". 
+// useSearchParams() should be wrapped in a suspense boundary at page "/404".
 // Read more: https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout
 
 import UserButton from "../UserButton/UserButton";
-import { useState } from "react"
+import { useState } from "react";
 
 export default function HeaderNav() {
-    const [menuOpen, setMenuOpen] = useState(false);
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+    // 모바일 메뉴 버튼
+    const mobileMenu = (
+        <button
+            onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+            className="sm:hidden mr-2 p-2 border rounded"
+        >
+            <FontAwesomeIcon icon={faBars} />
+        </button>
+    );
+
+    // home icon
+    const homeIcon = (
+        <Link href="/" className="ml-2 " aria-label="home">
+            <FontAwesomeIcon size="xl" icon={faHouse} aria-label="home" className="text-zinc-200" />
+        </Link>
+    );
+
+    // 모바일 드롭다운 메뉴
+    const mobileDropDownMenu = (
+        <div className="absolute left-0 top-full bg-gray-700 flex flex-col items-start p-4 sm:hidden">
+            <div className="flex justify-between items-center mt-2 gap-4">
+                <p>test1</p>
+                <p>test2</p>
+            </div>
+        </div>
+    );
 
     return (
         <header className="w-full bg-gray-800 text-white sticky top-0 z-100">
             <div className=" max-w-3xl mx-auto text-sm flex justify-between items-center p-2 relative ">
                 <div className="flex justify-between items-center gap-4">
-                    {/* 모바일 메뉴 버튼 */}
-                    <button
-                        onClick={() => setMenuOpen(!menuOpen)}
-                        className="sm:hidden mr-2 p-2 border rounded"
-                    >
-                        <FontAwesomeIcon icon={faBars} />
-                        {/* ☰ */}
-                    </button>
-                    <Link href="/" className="ml-2 " aria-label="home">
-                        <FontAwesomeIcon
-                            size="xl"
-                            icon={faHouse}
-                            aria-label="home"
-                            className="text-zinc-200"
-                        />
-                    </Link>
+                    {mobileMenu}
+                    {homeIcon}
                 </div>
                 <Link href="/qna" className="text-base hover:underline mr-12 p-2">
                     Q&A
@@ -45,16 +58,7 @@ export default function HeaderNav() {
                 <div className=" ">
                     <UserButton />
                 </div>
-
-                {/* 모바일 드롭다운 메뉴 */}
-                {menuOpen && (
-                    <div className="absolute left-0 top-full bg-gray-700 flex flex-col items-start p-4 sm:hidden">
-                        <div className="flex justify-between items-center mt-2 gap-4">
-                            <p>test1</p>
-                            <p>test2</p>
-                        </div>
-                    </div>
-                )}
+                {mobileMenuOpen && mobileDropDownMenu}
             </div>
         </header>
     );
