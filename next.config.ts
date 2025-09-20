8b68dfea09507323846dd84236d1f9ebaf016e78
jung hyun, ko <jeremyko69@gmail.com>
2025-09-21 00:07:35 +0900

    bodySizeLimit: "4mb" 변경

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,12 @@ const nextConfig: NextConfig = {
     eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
-
         // ignoreDuringBuilds: true,
+    },
+    experimental: {
+        serverActions: {
+            bodySizeLimit: "4mb",
+        },
     },
 };
 
